5220120294b5b9644d5b351215537f342e7998dc
unknown <aarena@su6-25.uninorteacad.uninorte.edu.co>
2016-08-24 20:09:35 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'); 
+var del = require('del'); 
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+
+gulp.task('clean',function(){
+    return del(['dist/**/*']);
+});
+
+gulp.task('build',['sass','images','fonts'],function(){
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/sass/**/*.*') 
+  .pipe(sass({includePaths: require('node-normalize-scss').includePaths}))
+  .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('src/fonts/**/*.*') 
+  .pipe(gulp.dest('dist/fonts'));
+});
+
+ 
+ gulp.task('images', function(){
+  gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images/'));
+});
+
+ gulp.task('default',['watch']);
+ 
+ gulp.task('watch', function () {
+  gulp.watch('src/sass/**/*.*', ['build']);
+});
